290d3a5fec3d5290c2f68cdd22f915c7fa1de57b
hydra <hydr414@gmail.com>
2022-11-16 02:22:29 +0300

    first commit

--- lib/screens/splash/components/splash_content.dart
+++ lib/screens/splash/components/splash_content.dart
@@ -17,7 +17,7 @@ class SplashContent extends StatelessWidget {
       children: <Widget>[
         Spacer(),
         Text(
-          "TOKOTO",
+          "BABAKRI STORE",
           style: TextStyle(
             fontSize: getProportionateScreenWidth(36),
             color: kPrimaryColor,
